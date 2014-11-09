3c3e30e492284c626eae2c88f96e119c35c26ac3
Alberto Rodríguez de la Cruz <albertrc.93@gmail.com>
2014-11-09 14:44:48 +0100

    Iniciando desarrollo principal...

---
+++ src/principal.c
@@ -0,0 +1,21 @@
+/*
+*	principal.c -> funcion main de PSO-ComeCocos
+*/	
+
+/*	Librerias  */
+#include <pthread.h>
+#include <stdio.h>
+
+/*
+ 	A la funcion main le pasamos el path del archivo de mapas
+*/
+int main(int argc, char * argv) {
+	
+	//Comprobamos numero de argumentos
+	if (argc != 2) 
+		error(1);
+	else {
+		//TODO
+	}
+	return 0;
+}
