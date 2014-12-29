0a0c9d254d4639d1dcaacc44b11d7070a7d61aeb
swordcito <sword_fk@hotmail.com>
2014-12-29 03:14:12 -0800

    Update jugador.c
    compila menos por la estructura del mapa

--- src/jugador.c
+++ src/jugador.c
@@ -1,39 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <ncurses.h>
+#include "estructuras.h"
 
-void jugador1 (int cambiocontroles, char controles1[4]){
+void controles1(char * right, char * left, char * up, char * down, char controlesj1[4]){
+
+*right = controlesj1[0];
+*left = controlesj1[1];
+*up = controlesj1[2];
+*down = controlesj1[3];
+}
+
+
+
+void jugador1 (int cambiocontroles, char controlesj1[4]){
 
 char right = 'd';
 char left = 'a';
 char up = 'w';
 char down = 's';
-char mov;
+char mov;//vble para guardar el caracter pulsado
+int dir= 0;//vble para el switch
 
 if(cambiocontroles != 0)
-	controles1(right, left, up, down, controles[4]);//si se han cambiado los controles del jugador uno, sustituye los que vienen por defecto
+	controles1(&right, &left, &up, &down, &controlesj1[4]);//si se han cambiado los controles del jugador uno, sustituye los que vienen por defecto
 cbreak();
 //flushinp();//vaciar buffer del teclado
 mov = getch();
 
-switch(mov){
-	case(down):if(mapa->jugador[1].y+1==' ') (mapa->jugador[1].y)+1;
+if(mov==right) dir = 1;
+else if (mov == left) dir = 2;
+else if (mov == up) dir = 3;
+else if (mov == down) dir = 4;
+
+switch(dir){
+	case(1):if(mapa->jugador[1].y+1==' ') (mapa->jugador[1].y)+1;
 	break;
-	case(up):if(mapa->jugador[1].y-1==' ') (mapa->jugador[1].y)-1;
+	case(2):if(mapa->jugador[1].y-1==' ') (mapa->jugador[1].y)-1;
 	break;
-	case(left):if(mapa->jugador[1].x-1==' ') (mapa->jugador[1].x)-1;
+	case(3):if(mapa->jugador[1].x-1==' ') (mapa->jugador[1].x)-1;
 	break;
-	case(right):if(mapa->jugador[1].x+1==' ') (mapa->jugador[1].x)+1;
+	case(4):if(mapa->jugador[1].x+1==' ') (mapa->jugador[1].x)+1;
 	break;
 	}
 }
 
-void controles1(char * right, char * left, char * up, char * down, char controles[4]){
-
-&right = controles1[0];
-&left = controles1[1];
-&up = controles1[2];
-&down = controles1[3];
-}
 
 /*habria que poner en el main un print preguntando si se quiere cambiar los controles, y si dice que si, poner cambiocontroles a 1 y rellenar la tabla de controles 1 con los nuevos controles*/
