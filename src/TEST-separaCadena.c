4064598e892ec8cf42b43aec5d49688ad5fa46a6
AzaharaP <azahara.ma.14@gmail.com>
2014-12-30 12:24:14 +0100

    Update TEST-separaCadena.c

--- src/TEST-separaCadena.c
+++ src/TEST-separaCadena.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-#define MAXCADENA 20
 void separaCadena(char cadena[],char*separadas[]);
-
 /*
 PRUEBA DE separaCadena.c
 Imprime por pantalla las cadenas previamente separadas
@@ -10,17 +10,30 @@ Imprime por pantalla las cadenas previamente separadas
 int main()
 {
 	int i=0;
+	
 	// Ejemplo de cadena
 	char cadena[]="Hola\nHoy es viernes\nAl año que viene nos mudamos\nTe quiero\nEstoy loca\nNo se que mas escribir\n";
+	
 	// Tabla de arrays donde guardamos las sub-cadenas
-	char * obtenida[MAXCADENA];
-	//Lamada a la funcion
-	separaCadena(cadena,obtenida);
-	// Imprimimos
-	while (obtenida[i] != NULL)
+	char ** obtenida = NULL;
+	obtenida=(char**)malloc(strlen(cadena));
+	// Comprobamos si se ha asignado la memoria correctamente
+	if (obtenida==NULL)
 	{
-		printf("%s\n",obtenida[i]);
-		i++;
+		printf("Error de memoria\n");
+	}
+	else
+	{
+		//Lamada a la funcion
+		separaCadena(cadena,obtenida);
+	
+		// Imprimimos
+		while (obtenida[i] != NULL)
+		{
+			printf("%s\n",obtenida[i]);
+			i++;
+		}
+		free(obtenida);
 	}
 	return 0;
 }
