93718a5741c97778b39c3e00856eba11bcd3b172
Javi <javi@HPjavi.(none)>
2014-12-30 10:24:33 +0100

    Nueva version fantasma

--- src/fantasma.c
+++ src/fantasma.c
@@ -5,22 +5,32 @@
 int i =1;
 char dir;
 int mov;
+int OK = 0;
 
 int fantasma()
 {
 mov = rand()%4;
 
+while(OK==0){
 	if(mov==0){
-		if ((mapa->fantasma[i].y)+1 == ' ') //Condición para poder subir
+		if ((mapa->fantasma[i].y)+1 == ' ' || mapa->fantasma[i].y)+1 == '-') //Condición para poder subir
 		       {
 			if(dir!='B')	{//Comprobamos
 				(mapa->fantasma[i].y)+1;//Nuestra variable tiene que ser distinta de B, para que no vuelva a bajar
-				dir = 'S';				
+				dir = 'S';
+				OK++;				
 					}
 			else	{
+				do
 				mov = rand()%4;
+				while(mov==0)
 				}
 		  	}
+		else	{
+			do
+			mov = rand()%4;
+			while(mov==0)
+		    	}
 		   }
 	
 	if(mov==1){
@@ -29,11 +39,19 @@ mov = rand()%4;
 			if(dir!='S')	{//Decimos que sea distinto de Subir para que no haga bucle
 	    			(mapa->fantasma[i].y)-1;
 				dir = 'B';
+				OK++;
 					}			
 			else	{
+				do
 				mov = rand()%4;
+				while(mov==1)
 		    		}
 			}
+		else	{
+			do
+			mov = rand()%4;
+			while(mov==1)
+		    	}
 		   }
 			
 	if(mov==2){
@@ -41,12 +59,20 @@ mov = rand()%4;
 			{
 			if(dir!='D')	{
 	    			(mapa->fantasma[i].x)+1;
-				dir = 'I';			
+				dir = 'I';
+				OK++;			
 					}
 			else	{
+				do
 				mov = rand()%4;
+				while(mov==2)
 	 	    		}
 			}
+		else	{
+			do
+			mov = rand()%4;
+			while(mov==2)
+		    	}
 		   }
 
 	if(mov==3){
@@ -54,12 +80,21 @@ mov = rand()%4;
 			{
 			if(dir!='I')	{
 	    			(mapa->fantasma[i].x)-1;
-				dir = 'D';			
+				dir = 'D';
+				OK++;			
 					}
 			else	{
+				do
 				mov = rand()%4;
+				while(mov==3)
 		    		}
 			}
-		   } 
+		else	{
+			do
+			mov = rand()%4;
+			while(mov==3)
+		    	}
+		   }
+} 
 return 0;
 }
