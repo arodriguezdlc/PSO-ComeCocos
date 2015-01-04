641426fc6f3eeb6ca958ebd4796ba6500e74f137
Alberto Rodríguez de la Cruz <albertrc.93@gmail.com>
2015-01-04 23:41:24 +0100

    desarrollado guardamapa

--- src/choque.c
+++ src/choque.c
@@ -24,10 +24,17 @@ while(j>0){
 return resultado;
 }
 
-void guardamapa(MAPA * mapa, MAPAANT * mapaAnt, int i, int j){
+void guardamapa(MAPA * mapa, MAPAANT * mapaAnt){
 
-
-
-
-
+	int i;
+	mapaAnt->numFantasmas = mapa->numFantasmas;
+	mapaAnt->numJugadores = mapa->numJugadores;
+	for(i = 0; i < mapa->numJugadores; i++) {
+		mapaAnt->jugador[i].x = mapa->jugador[i].x;
+		mapaAnt->jugador[i].y = mapa->jugador[i].y;
+	}
+	for(i = 0; i < mapa->numFantasmas; i++) {
+		mapaAnt->fantasma[i].x = mapa->fantasma[i].x;
+		mapaAnt->fantasma[i].y = mapa->fantasma[i].y;
+	}
 }
