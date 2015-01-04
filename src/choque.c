df72439ba0539089f20fd1343295c6effa01748f
juanan <sword_fk@hotmail.com>
2015-01-04 14:24:02 -0800

    choque

--- src/choque.c
+++ src/choque.c
@@ -9,12 +9,25 @@
 #define SI 1
 
 
-int choque(MAPA * mapa, int i, int j){//funcion para ver si chocan el jugador y el fantasma
+int choque(MAPA * mapa, MAPAANT * mapaAnt, int i, int j){//funcion para ver si chocan el jugador y el fantasma
 
 int resultado = NO;
-
-if(((mapa->jugador[j].x)==(mapa->fantasma[i].x))&&((mapa->jugador[j].y)==(mapa->fantasma[i].y)))
-resultado=SI;
+while(j>0){
+	while (i>0){
+		if(((mapa->jugador[j].x)==(mapa->fantasma[i].x))&&((mapa->jugador[j].y)==(mapa->fantasma[i].y)))
+		resultado=SI;
+		i--;
+		}
+	j--;
+	}
 
 return resultado;
+}
+
+void guardamapa(MAPA * mapa, MAPAANT * mapaAnt, int i, int j){
+
+
+
+
+
 }
