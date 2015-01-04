2d3026788602371e238445faa51949065d96b7c1
juanan <sword_fk@hotmail.com>
2015-01-04 15:38:22 -0800

    --

--- src/choque.c
+++ src/choque.c
@@ -13,8 +13,10 @@
 int choque(MAPA * mapa, MAPAANT * mapaAnt, int i, int j){//funcion para ver si chocan el jugador y el fantasma
 
 int resultado = NO;
-while(j>0){
-	while (i>0){
+j--;
+i--;
+while(j>=0){
+	while (i>=0){
 		if(((mapa->jugador[j].x)==(mapa->fantasma[i].x))&&((mapa->jugador[j].y)==(mapa->fantasma[i].y)))
 		resultado=SI;
 		if(((mapaAnt->jugador[j].x)==(mapa->fantasma[i].x))&&((mapaAnt->jugador[j].y)==(mapa->fantasma[i].y)))
